ebca7b6f93a853006525bea2e86cabf24d31b40a
Kipyegon Wycliffe Langat <KLANGAT@safaricom.co.ke>
2022-01-03 23:31:19 +0300

    introducing a function toggle on click

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 console.log("Testing")
 
+$(document).ready(function() {
+    $("img").click(function() {
+      $(".design-showing").toggle();
+      $(".design-hiden").toggle();
+    });
+    $("p").click(function(){
+    $(".design-showing").toggle();
+    $(".design-hiden").toggle();
+    });
+  });
